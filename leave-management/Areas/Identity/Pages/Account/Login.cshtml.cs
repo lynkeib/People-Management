c4ac1581ed4f969bd5f06837f46fc51ad69f2071
Chengyin Liu <liucy9511@gmail.com>
2020-04-15 21:42:04 -0700

    Test

--- leave-management/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ leave-management/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace leave_management.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Remember me?")]//a
             public bool RememberMe { get; set; }
         }
 
