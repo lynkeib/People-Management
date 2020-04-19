d7875679559be1c9224370229954f2185204528b
lynkeib <liuchengyin1234@126.com>
2020-04-19 14:50:40 -0700

    Requests Processing

--- leave-management/Models/LeaveRequestVM.cs
+++ leave-management/Models/LeaveRequestVM.cs
@@ -1,31 +1,32 @@
-﻿using leave_management.Data;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace leave_management.Models
 {
-    public class LeaveHistoryVM
+    public class LeaveRequestVM
     {
         public int Id { get; set; }
         public EmployeeVM RequestingEmployee { get; set; }
         public string RequestingEmployeeId { get; set; }
-
-        [Required]
         public DateTime StartDate { get; set; }
-        [Required]
         public DateTime EndDate { get; set; }
         public LeaveTypeVM LeaveType { get; set; }
-        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
         public int LeaveId { get; set; }
         public DateTime DateRequested { get; set; }
         public DateTime DateActioned { get; set; }
         public bool? Approved { get; set; }
         public EmployeeVM ApprovedBy { get; set; }
         public string ApprovedById { get; set; }
-       
+    }
+
+    public class AdminLeaveRequestsViewVM
+    {
+        public int TotalRequests { get; set; }
+        public int ApprovedRequests { get; set; }
+        public int PendingRequests { get; set; }
+        public int RejectedRequests { get; set; }
+        public List<LeaveRequestVM> LeaveReuqests { get; set; }
     }
 }
--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -37,7 +37,7 @@ namespace leave_management
 
             // Dependency Injection
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
-            services.AddScoped<ILeaveHistoryRepository, LeaveHistoryRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
             services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
             // AutoMapper
             services.AddAutoMapper(typeof(Maps));
